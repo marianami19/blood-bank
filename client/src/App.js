0efb3af0df5c14ddbc32734f454ce3bfbe91aad5
marianami19 <45075042+marianami19@users.noreply.github.com>
2023-03-30 09:35:16 +0530

    restrcuture

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
+// import Login from './Login';
+import Request from './Request';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      {/* <Login></Login> */}
+      <Request></Request>
     </div>
   );
 }
---
+++ client/src/Request.js
@@ -0,0 +1,22 @@
+
+function Request() {
+    return (
+        <div className="container">
+            <h1 className='display-6 text-center'>Available blood samples</h1>
+            <form>
+                <div>
+                    <ul class="list-group">
+                        <li class="list-group-item active">Cras justo odio  <button class="btn btn-outline-secondary" type="button">Button</button>
+</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Morbi leo risus</li>
+                        <li class="list-group-item">Porta ac consectetur ac</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Request;
